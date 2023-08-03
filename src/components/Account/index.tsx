2ae0710fa58c4f236610724a85197fb84ad4ff0b
johnCena___ <john.cena@alfextrading.com>
2023-08-03 14:30:40 +0300

    Account form inputs

--- src/components/Account/index.tsx
+++ src/components/Account/index.tsx
@@ -1,14 +1,31 @@
 import User from '../../stores/user.ts';
 import './style.css';
 import { mdiFileEditOutline } from '@mdi/js';
+import { useState, useEffect } from 'react';
+import { observer } from 'mobx-react';
 import Icon from '@mdi/react';
+import { IUser } from '../../types/user.ts';
 
 const userStore = User;
 
-const Account = () => {
-  const user = userStore.getUser();
+const Account = observer(() => {
+  const user: IUser | null = userStore.getUser();
 
-  return (
+  const [firstName, setFirstName] = useState(user?.firstName);
+  const [lastName, setLastName] = useState(user?.lastName);
+  const [phone, setPhone] = useState(user?.phone);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    setIsLoaded(!!user);
+    if (user) {
+      setPhone(user?.phone);
+      setLastName(user?.lastName);
+      setFirstName(user?.firstName);
+    }
+  }, [userStore.user]);
+
+  return (isLoaded &&
     <div className={'account'}>
       <h2 className={'account__header'}>
         Account information
@@ -31,8 +48,59 @@ const Account = () => {
           <p className={'ml-2'}>Edit</p>
         </div>
       </div>
+
+      <form className={'account__form'}>
+        <div className={'app__form-row'}>
+          <div className={'app__form-field w-[45%]'}>
+            <span className={'app__form-lbl'}>
+              First Name
+            </span>
+            <input
+              className={'app-input !w-full'}
+              type={'text'}
+              value={firstName || ''}
+              onChange={(event) => setFirstName(event.target.value)}
+            />
+          </div>
+
+          <div className={'app__form-field w-[45%]'}>
+            <span className={'app__form-lbl'}>
+              Last Name
+            </span>
+            <input
+              className={'app-input !w-full'}
+              type={'text'}
+              value={lastName || ''}
+              onChange={(event) => setLastName(event.target.value)}
+            />
+          </div>
+        </div>
+
+        <div className={'app__form-row'}>
+          <div className={'app__form-field w-[45%]'}>
+            <span className={'app__form-lbl'}>
+              Phone
+            </span>
+            <input
+              className={'app-input !w-full'}
+              type={'text'}
+              value={phone || ''}
+              onChange={(event) => setPhone(event.target.value)}
+            />
+          </div>
+
+          <div className={'app__form-field w-[45%]'}>
+            <span className={'app__form-lbl'}>
+              Email
+            </span>
+            <div className={'app-fake-input !w-full'}>
+              {user?.email}
+            </div>
+          </div>
+        </div>
+      </form>
     </div>
   )
-};
+});
 
 export default Account;
