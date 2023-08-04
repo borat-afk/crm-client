300e606adeb8f7222012eab08cbd9f8ba4ef4372
johnCena___ <john.cena@alfextrading.com>
2023-08-04 18:00:10 +0300

    Pagination conf

--- src/components/Users/index.tsx
+++ src/components/Users/index.tsx
@@ -1,19 +1,40 @@
 import { usersTableColumnsConst } from '../../constants/users-table-columns.const.ts';
 import { observer } from 'mobx-react-lite';
 import { IUser } from "../../types/user.ts";
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { DateTime } from 'luxon';
 import { userStatusFilter } from '../../filters/user-status.filter.ts';
-import Table from 'rc-table';
+import { MaterialReactTable, type MRT_ColumnDef } from 'material-react-table';
+import Select from 'react-select';
 import UsersStore from '../../stores/users.ts';
+import ResponsivePagination from 'react-responsive-pagination';
+import 'react-responsive-pagination/themes/bootstrap.css';
 import './style.css';
 
 const usersStore = UsersStore;
 
+const paginationPerPageOptions = [
+  { value: 5, label: '5' },
+  { value: 10, label: '10' },
+  { value: 15, label: '15' },
+  { value: 20, label: '20' },
+  { value: 25, label: '25' },
+  { value: 50, label: '50' },
+]
+
 const Users = observer(() => {
   const [users, setUsers] = useState<IUser[] | null | undefined>(null);
   const [page, setPage] = useState<number>(1);
-  const [perPage, setPerPage] = useState<number>(10);
+  const [pageCount, setPageCount] = useState<number>(2);
+  const [rowCount, setRowCount] = useState<number>(0);
+  const [perPage, setPerPage] = useState<{ value: number, label: string }>({ value: 5, label: '5' });
+
+  const columns = useMemo<MRT_ColumnDef<IUser>[]>(
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    () => usersTableColumnsConst,
+    []
+  );
 
   useEffect(() => {
     (async () => {
@@ -32,6 +53,7 @@ const Users = observer(() => {
           return item;
         });
         setUsers(tableData);
+        setRowCount(usersStore.getTotal());
       } catch (e) {
         throw new Error();
       }
@@ -41,7 +63,31 @@ const Users = observer(() => {
   return (
     <div>
       <h2>Users</h2>
-      {users && <Table columns={usersTableColumnsConst} data={users} rowKey={'dataIndex'}/>}
+      {users && <MaterialReactTable
+        columns={columns}
+        data={users}
+        enableClickToCopy
+        enablePagination={false}
+      />}
+      {users && pageCount > 1 && <div className={'w-full flex justify-end mt-2'}>
+        <div className={'flex items-center mr-4'}>
+          <span className={'text-primary font-light text-lg mr-2'}>
+            Items per page:
+          </span>
+          <Select
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            onChange={(newPerPage) => setPerPage(newPerPage)}
+            options={paginationPerPageOptions}
+            value={perPage}
+          />
+        </div>
+        <ResponsivePagination
+          current={page}
+          total={pageCount}
+          onPageChange={(page: number) => setPage(page)}
+        />
+      </div>}
     </div>
   )
 });
--- src/stores/users.ts
+++ src/stores/users.ts
@@ -4,26 +4,53 @@ import { IUser } from '../types/user.ts';
 
 class UsersStore {
   users: IUser[] | null = null;
+  total: number = 0;
+  perPage: number | null = null;
+  skip: number | null = null;
 
   constructor() {
     makeObservable(this, {
       users: observable,
+      total: observable,
+      perPage: observable,
+      skip: observable,
+      setPerPage: action,
+      setSkip: action,
       setUsers: action
     })
+  }
+
+  setPerPage(newPerPage: number) {
+    this.perPage = newPerPage;
+  }
+
+  setSkip(newSkip: number) {
+    this.skip = this.perPage? newSkip * this.perPage : null;
   }
 
   setUsers(usersList: IUser[]) {
     this.users = [...usersList];
   }
 
+  setTotal(newTotal: number) {
+    this.total = newTotal;
+  }
+
   getUsers(): IUser[] | null {
     return this.users;
   }
 
+  getTotal(): number {
+    return this.total;
+  }
+
   async fetchUsers() {
     try {
-      const res = await api.get('/user');
-      this.setUsers(res?.data);
+      // limit skip
+      const paginationQuery = `?limit=${this.perPage}&skip=${this.skip}`
+      const res = await api.get(`/user${this.perPage && this.skip ? paginationQuery : '' }`);
+      this.setUsers(res?.data.data);
+      this.setTotal(res?.data.total);
     } catch (e) {
       throw new Error();
     }
